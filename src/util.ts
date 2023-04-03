0cfeaa4b8ef12e2df3a33a9830147670622fbe0e
rhaoio <richard@rhao.io>
2023-04-03 18:38:04 +1000

    add eth msg prefix

--- src/util.ts
+++ src/util.ts
@@ -1,8 +1,10 @@
-import EthCrypto from 'eth-crypto';
+import EthCrypto from "eth-crypto";
 
 export function signMessage(message: string): string {
-    //Currently not working with B/E validation
-    const messageHash = EthCrypto.hash.keccak256(message);
-    let priv = "";
-    return EthCrypto.sign(priv, messageHash);
+  //Currently not working with B/E validation
+  let msg_len = message.length;
+  const ETH_PREFIX = "\x19Ethereum Signed Message:\n" + msg_len;
+  const messageHash = EthCrypto.hash.keccak256(ETH_PREFIX + message);
+  let priv = "";
+  return EthCrypto.sign(priv, messageHash);
 }
