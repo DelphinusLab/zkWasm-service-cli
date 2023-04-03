63ed4668a3831c274c9b2c58597774f5226565e4
yymone <yymone84@hotmail.com>
2023-04-03 15:59:01 +1000

    remove priv

--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,8 @@
 import EthCrypto from 'eth-crypto';
 
 export function signMessage(message: string): string {
+    //Currently not working with B/E validation
     const messageHash = EthCrypto.hash.keccak256(message);
-    let priv = "917b1dc01120430c3f7c15b653e9b2278115b1b3a943e4f092c7aa079b0b3f3d";
+    let priv = "";
     return EthCrypto.sign(priv, messageHash);
 }
