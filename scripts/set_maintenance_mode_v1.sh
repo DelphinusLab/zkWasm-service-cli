506dddb2dfc220d1e17a02536d4788a6d1f5f068
qozymandias <odowning19@gmail.com>
2024-07-26 14:44:28 +0800

    rm unneeded scripts

--- scripts/set_maintenance_mode_v1.sh
+++
@@ -1,5 +0,0 @@
-node dist/index.js setmaintenancemode \
-    -r "http://127.0.0.1:8108" \
-    -u "0x3552f5E0BFcCF79A87a304e80455b368Af9B56F6" \
-    -x "3c873c694cfbde6f803b428808911d7e55ce7c0d74e71b33f48b1eb7efc519bb" \
-    --active true;
