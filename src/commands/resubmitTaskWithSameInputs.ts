8df32ee472a4dcad694e35df14d0195c291fa8c0
qozymandias <odowning19@gmail.com>
2025-04-10 17:14:57 +1000

    fix

--- src/commands/resubmitTaskWithSameInputs.ts
+++ src/commands/resubmitTaskWithSameInputs.ts
@@ -15,13 +15,15 @@ export const builder = (yargs: Argv) => {
     })
     .option("taskids", {
       describe: "List of task ids to resubmit proofs for.",
+      demandOption: "The task ids are required.",
       type: "array",
+      string: true,
       default: [],
     });
 };
 
 export const handler = async (argv: Arguments) => {
-  console.log("Begin reset image task for ", argv.i);
+  console.log("Begin resubmit tasks with same inputs");
   await resubmitTaskWithSameInputs(
     argv.r as string,
     argv.x as string,
