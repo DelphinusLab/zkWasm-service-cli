e6d56e8060a98ae215f68f21d2f0c83aa5a7af12
qozymandias <odowning19@gmail.com>
2025-07-25 16:51:39 +1000

    added

--- tests/run_production_proof_samples.sh
+++ tests/run_production_proof_samples.sh
@@ -5,7 +5,7 @@
 #   /home/yymone/yyu/ExplorerServerBackup/RocksDB/e2e/rocksdb-production-samples-for-e2e.tar.gz
 
 # Script for running extra tests first time. Cli will be built, image will be added and script will wait for everything to finish.
-bash tests/scripts/add_multiple_proofs.sh true "tests/inputs/production.json"
+bash scripts/cli/scripts/add_multiple_proofs.sh true "scripts/cli/inputs/production.json"
 
 # Script for running extra tests after the first time. Image add will be skipped and script will not wait for proof to finish.
-# bash tests/scripts/add_multiple_proofs.sh false "tests/inputs/production.json" false false false
+# bash scripts/cli/scripts/add_multiple_proofs.sh false "scripts/cli/inputs/production.json" false false false
